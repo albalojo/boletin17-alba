82bb1ae43b66b23389f0a01c86bfece833cbd042
OLIMPIA\dam113 <albalojo98@gmail.com>
2017-05-04 10:23:29 +0200

    Ver. 2

---
+++ Boletin17-Junit/src/boletin17/DNI.java
@@ -0,0 +1,19 @@
+package boletin17;
+
+import java.util.ArrayList;
+
+public class DNI {
+
+	boolean eValido(String numeroLetra) {
+		return false;
+	}
+	int calculaLetra(String numero) {
+		return 0;
+	}
+	boolean eValido (ArrayList<Integer> numeros, char letra) {
+		return false;
+	}
+	int calculaLetra(ArrayList<Integer> numeros) {
+		return 0;
+	}
+}
---
+++ Boletin17-Junit/src/boletin17/Xeometria.java
@@ -0,0 +1,18 @@
+package boletin17;
+
+public class Xeometria {
+	
+	int perímetroCadrado(int lado) {
+		return 0;
+	}
+	int areaCadrado(int lado) {
+		return 0;
+	}
+	double teoremaPitagoras(int catetoA, int catetoB) {
+		return 0;
+	}
+	double teoremaPitagoras (double hipotenusa, double catetoA){
+		return catetoA;
+		
+	}
+}
