837a378e7886dfd91b229e9e866813efa9b19115
OLIMPIA\dam113 <albalojo98@gmail.com>
2017-05-16 14:16:49 +0200

    Ver. 7

--- Boletin17-Junit/src/boletin17/Xeometria.java
+++ Boletin17-Junit/src/boletin17/Xeometria.java
@@ -6,22 +6,23 @@ package boletin17;
 import java.lang.Math;
 
 public class Xeometria {
+	//Perímetro
 	public static int perimetroCadrado(int lado) {
 		int perimetro;
 		perimetro = lado * 4;
 		return perimetro;
 	}
-
+	//Área
 	public int areaCadrado(int lado) {
 		if (lado < 0)
 			return -1;
 		return lado * 2;
 	}
-
+	//Hipotenusa
 	public double teoremaPitagoras(int catetoA, int catetoB) {
 		return Math.sqrt((catetoA * catetoA) + (catetoB * catetoB));
 	}
-
+	//Catetos
 	public double teoremaPitagoras(double catetoA, double hipotenusaC) {
 		return Math.sqrt((hipotenusaC * hipotenusaC) - (catetoA * catetoA));
 
